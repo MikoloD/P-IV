bd27b0474bc14a9018b4ca8a3e8be07d74bc2b7b
MikoloD <grygiel.mikolaj@gmail.com>
2020-05-12 21:39:51 +0200

    lab10

---
+++ WPF_Bind/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Bind
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Student simpleStudent = new Student()
+        {
+            Id = 1,
+            Name = "Jan",
+            LastName = "Kowalski",
+            EnlistingDate = new DateTime(2015, 10, 1)
+        };
+
+        public List<Student> students = new List<Student>()
+        {
+            new Student()
+            {
+            Id = 1,
+            Name = "Jan",
+            LastName = "Kowalski",
+            EnlistingDate = new DateTime(2015, 10, 1)
+            },
+             new Student()
+            {
+            Id = 2,
+            Name = "Jan",
+            LastName = "Nowak",
+            EnlistingDate = new DateTime(2016, 10, 1)
+            },
+              new Student()
+            {
+            Id = 3,
+            Name = "Jan",
+            LastName = "Kwiatkowski",
+            EnlistingDate = new DateTime(2017, 10, 1)
+            },
+               new Student()
+            {
+            Id = 4,
+            Name = "Jan",
+            LastName = "Smith",
+            EnlistingDate = new DateTime(2018, 10, 1)
+            },
+        };
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = simpleStudent;
+            t2.SetBinding(TextBox.TextProperty, new Binding("Name"));
+
+            StudnetLV.ItemsSource = students;
+            StudentLB.ItemsSource = students;
+        }
+    }
+}
---
+++ WPF_Bind/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Bind
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string FullName => $"{Name} {LastName}";
+        public DateTime EnlistingDate { get; set; }
+
+        public override string ToString()
+        {
+            return "[STUDENT]";
+        }
+    }
+}
