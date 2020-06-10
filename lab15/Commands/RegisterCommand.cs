40fb4533efa2cab9530daadc083d86ae46d7fd17
MikoloD <grygiel.mikolaj@gmail.com>
2020-06-10 14:58:32 +0200

    lab15

---
+++ lab15/Commands/RegisterCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace lab15.Commands
+{
+    public class RegisterCommand : ICommand
+    {
+        private RegisterViewModelValidator validator = new RegisterViewModelValidator();
+
+        public event EventHandler CanExecuteChanged
+        {
+           add { CommandManager.RequerySuggested += value; }
+           remove { CommandManager.RequerySuggested -= value; }
+        }
+        public bool CanExecute(object parameter)
+        {
+            var model = parameter as RegisterViewModel;
+            if(model is null) return false;
+            var resoults = validator.Validate(model);
+            model.Errors = string.Join(" ", resoults.Errors);
+            return resoults.IsValid;
+        }
+
+        public void Execute(object parameter)
+        {
+            var model = parameter as RegisterViewModel;
+            MessageBox.Show($"Zarejstrowano urzytkownika {model.Email}", "Powodzenie", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
---
+++ lab15/MainWindowViewModel.cs
@@ -0,0 +1,118 @@
+﻿using FluentValidation;
+using lab15.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace lab15
+{
+    public class MainWindowViewModel
+    {
+        public MainWindowViewModel()
+        {
+            Model = new RegisterViewModel();
+            RegisterCommand = new RegisterCommand();
+        }
+        public RegisterViewModel Model { get; set; }
+        public ICommand RegisterCommand { get; set; }
+    }
+
+    public class RegisterViewModelValidator : AbstractValidator<RegisterViewModel>
+    {
+        public RegisterViewModelValidator()
+        {
+            CascadeMode = CascadeMode.StopOnFirstFailure;
+
+            RuleFor(x => x.Email)
+                .NotNull()
+                .NotEmpty()
+                .EmailAddress();
+
+            RuleFor(x => x.Password)
+                .NotNull()
+                .NotEmpty()
+                .MinimumLength(6)
+                .Must(x => x != x.ToLower())
+                .WithMessage("Hasło musi zawierc przynajmniej jedną wielką literę")
+                .Must(x => x != x.ToUpper())
+                .WithMessage("Hasło musi zawierc przynajmniej jedną małą literę")
+                .Matches(@".*\d.*")
+                .WithMessage("Hasło musi zawierc conajmniej jedna cyfre");
+
+            RuleFor(x => x.IsHuman)
+                .Must(x => x)
+                .WithMessage("Music potwierdzic że jestes czlowiekiem");
+        }
+    }
+
+    public class RegisterViewModel : INotifyPropertyChanged
+    {
+        private string _email;
+
+        public string Email
+        {
+            get { return _email; }
+            set { 
+                if(_email !=value)
+                {
+                    _email = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Email"));
+                }
+            
+            }
+        }
+
+        private string _password;
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                if (_password != value)
+                {
+                    _password = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Password"));
+                }
+
+            }
+        }
+        private bool _isHuman;
+
+        public bool IsHuman
+        {
+            get { return _isHuman; }
+            set
+            {
+                if (_isHuman != value)
+                {
+                    _isHuman = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsHuman"));
+                }
+
+            }
+        }
+        private string _errors;
+
+        public string Errors
+        {
+            get { return _errors; }
+            set
+            {
+                if (_errors != value)
+                {
+                    _errors = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Errors"));
+                }
+
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
